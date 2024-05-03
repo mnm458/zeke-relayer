c2e11573a246a0afe3cf28104f126c27e572b4ea
Mohak <mohaknainmalhotra@gmail.com>
2024-05-03 23:10:09 +1000

    chore: refactor basic event listener

--- main.go
+++ main.go
@@ -3,23 +3,37 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
 )
 
-type Chains struct {
-	Base string `json`
-}
-
 func main() {
-	if os.Args[1] == "base" {
 
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
 	}
-	const url = "wss://rpc.ankr.com/base/ws/5b3e494193a39bb51d06eb3cb4735c305870850adcd0987f03bd6f88ea308737" // url string
+	if len(os.Args) < 2 {
+		log.Fatal("No chain argument provided")
+	}
 
+	args := os.Args[1]
+
+	apiKey := os.Getenv("ANKR_API_KEY")
+
+	var url string
+	if args == "base" {
+		url = fmt.Sprintf("wss://rpc.ankr.com/%s_sepolia/ws/%s", "base", apiKey)
+	} else {
+		url = fmt.Sprintf("wss://rpc.ankr.com/%s_sepolia/ws/%s", "mantle", apiKey)
+	}
+	fmt.Println(url)
 	client, err := ethclient.Dial(url)
 
 	if err != nil {
